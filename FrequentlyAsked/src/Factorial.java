1365ed0d7357b32d390b69b6cca010c8df282914
Nandakumar TS <55013023+nandu10154@users.noreply.github.com>
2024-03-26 12:36:05 +0530

    Factorial of a number

--- FrequentlyAsked/src/Factorial.java
+++ FrequentlyAsked/src/Factorial.java
@@ -1,11 +1,14 @@
 public class Factorial {
     public static int fact(int n){
-        int fact =1;
-        for(int i =1;i<=n;i++){
-            fact = fact*i;
+        if ((n==1) || (n==0 )){
+            return 1;
+        }else{
+            int fact = 1;
+            for (int i = 1; i <= n; i++) {
+                fact = fact * i;
+            }
+            return fact;
         }
-        return fact;
-
     }
 
     public static void main(String[] args) {
